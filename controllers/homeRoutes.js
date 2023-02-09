7ff11bd9a51bf348a11c68cad21c0cd03a66fbb1
na-br-wo <nathan.b.wood@gmail.com>
2023-02-08 20:20:52 -0700

    added login and links

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,25 +2,25 @@ const router = require('express').Router();
 const { Recipe, Ingredient, Tag } = require('../models');
 
 router.get('/', async (req, res) => {
-    try {
-      // Get all projects and JOIN with user data
-  const recipeData = await Recipe.findAll()
-      // Serialize data so the template can read it
-      const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
-  
-      // Pass serialized data and session flag into template
-      res.render('homepage', { 
-        recipes, 
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+  try {
+    // Get all projects and JOIN with user data
+    const recipeData = await Recipe.findAll()
+    // Serialize data so the template can read it
+    const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      recipes,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/recipe/:id', async (req, res) => {
   try {
     const recipeData = await Recipe.findByPk(req.params.id, {
-      include: [{model:Ingredient},{model:Tag}]
+      include: [{ model: Ingredient }, { model: Tag }]
     });
     const recipe = recipeData.get({ plain: true });
     console.log(recipe)
@@ -32,4 +32,13 @@ router.get('/recipe/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-  module.exports = router;
+
+router.get('/login', async (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login')
+});
+
+module.exports = router;
