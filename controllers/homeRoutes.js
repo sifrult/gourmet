71aef3b594f5948594d0b9c5f289656563665e60
na-br-wo <nathan.b.wood@gmail.com>
2023-02-08 19:46:54 -0700

    can display recipe name

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
     }
   });
 
-router.get('/recipe/;id', async (req, res) => {
+router.get('/recipe/:id', async (req, res) => {
   try {
     const recipeData = await Recipe.findByPk(req.params.id, {
       include: [{model:Ingredient},{model:Tag}]
