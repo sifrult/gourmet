ca31a6a55b785ac577c9b111947adf56d1f9b29b
Amy Snarr <amy.snarr16@gmail.com>
2023-02-10 08:55:23 -0700

    clicking on recipe name takes you to the recipe

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
     const recipeData = await Recipe.findAll()
     // Serialize data so the template can read it
     const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
-
+    console.log(recipes)
     // Pass serialized data and session flag into template
     res.render('homepage', {
       recipes,
